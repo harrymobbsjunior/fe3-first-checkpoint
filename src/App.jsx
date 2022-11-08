5202c42cecf8f1ddd7e60fc863a177a9917f2518
Harry Möbbs Júnior <harrymobbsjunior@gmail.com>
2022-11-07 21:58:03 -0300

    Primeiro commit estrutura base.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+// Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+import { Card } from './Card'
+
+function App() {
+  // Aqui você irá criar os Estados para manipular os Inputs
+  
+
+  return (
+    <div className="App">
+     <h1>Carga de estudiantes</h1>
+     <form>
+      {/* Comece a desenvolver o seu Código por aqui :) */}
+
+        <Card />
+     </form>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+//Este componente deverá receber dados por Props e mostrar as Informações em Tela
+
+export function Card () {
+  return (
+    <div>
+        <div className="boxCard">
+          <h3>Cor</h3>
+          <p>Código</p>
+        </div>
+    </div>
+  )
+}
