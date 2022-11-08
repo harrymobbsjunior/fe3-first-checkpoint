e2dacd6b3fe01bd1dc103d3772658e20d17fd0c8
Harry Möbbs Júnior <harrymobbsjunior@gmail.com>
2022-11-07 22:47:46 -0300

    atualização primeira parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
 
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+import { useState } from 'react'
 import { Card } from './Card'
+import { Button } from 'react-bootstrap';
+import './style.scss'
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
-  
+  const [nomeCor, setNomeCor] = useState();
+  const [corHexadecimal, setCorHexadecimal] = useState();
+
+  function adicionarCor(event) {
+    event.preventDefault()
+    const coloracao = {
+      nomeCor: nomeCor,
+      hexadecimal: corHexadecimal
+    }
+	
+	setAllCores([...allCores])
+
+    //console.log(coloracao)
+  }
 
   return (
     <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
+     <h1>SISTEMA DE CORES</h1>
+     <form className="formCadastro"  onSubmit={event => adicionarCor(event)}>
+		<h2>CADASTRO DE COR</h2>
 
-        <Card />
-     </form>
+		<input type="text" className="form-control" value={nomeCor} onChange={event => setNomeCor(event.target.value)} placeholder="Entre com o nome da Cor"/>
+
+		<input type="color" className="form-control" value={corHexadecimal} onChange={event => setCorHexadecimal(event.target.value)} placeholder="Escolha a cor"/>
+
+		<button type="submit" className="btn btn-primary">Adicionar</button>
+	</form>
+
+	<section className="CardColor">
+		{
+			allColors.map (
+				color => {
+
+				}
+			)
+		}
+	</section>
+     {/* <Card /> */}
     </div>
+
   )
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,18 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
     <div>
         <div className="boxCard">
-          <h3>Cor</h3>
+          <h3>Color</h3>
           <p>Código</p>
+        </div>
+
+        <div className="boxCardItem">
+            <div className="card-body">
+                <h1>{props.productData.name}</h1>
+                <span>R$ {props.productData.price},00</span>
+            </div>
         </div>
     </div>
   )
